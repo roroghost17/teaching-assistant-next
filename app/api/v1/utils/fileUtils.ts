818e35a8165d1a75a7a1df8ed5485b130003f290
roroghost17 <roroghost17@gmail.com>
2026-01-23 03:40:54 +0530

    Replaces pdf-parse with unpdf

--- app/api/v1/utils/fileUtils.ts
+++ app/api/v1/utils/fileUtils.ts
@@ -1,13 +1,14 @@
-import fs from 'fs';
-import { PDFParse } from 'pdf-parse'
+import { readFile } from 'node:fs/promises';
+import { extractPDFText, getDocumentProxy } from 'unpdf';
 
 export const readPdfContent = async (filePath: string): Promise<string> => {
   try {
-    const dataBuffer = fs.readFileSync(filePath);
-    const parser = new PDFParse(dataBuffer);
-    const data = await parser.getText();
-
-    return data.text;
+    const buffer = await readFile(filePath);
+    const pdf = await getDocumentProxy(new Uint8Array(buffer));
+    const { text } = await extractPDFText(pdf, { mergePages: true });
+    const textString = typeof text === 'string' ? text : text.join(' ');
+    const cleanedText = textString.replace(/\s+/g, ' ').trim();
+    return cleanedText;
   } catch (error) {
     console.error('Error reading PDF file:', error);
     throw new Error('Failed to read PDF content');
